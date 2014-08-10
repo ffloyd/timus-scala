c5e58398dcac704d9648530e742a642d7ba06595
Roman Kolesnev <rvkolesnev@gmail.com>
2014-08-11 03:23:37 +0400

    Problem 1002: Phone numbers Accepted solution. Needs refactoring.

--- src/Problem1002.scala
+++ src/Problem1002.scala
@@ -1,51 +1,105 @@
 import scala.annotation.tailrec
+import scala.collection.mutable
 
-object Problem1002 {
-  def wordToNumbers(word: String): String = {
-    word.map {
-      case 'i' | 'j'        => '1'
-      case 'a' | 'b' | 'c'  => '2'
-      case 'd' | 'e' | 'f'  => '3'
-      case 'g' | 'h'        => '4'
-      case 'k' | 'l'        => '5'
-      case 'm' | 'n'        => '6'
-      case 'p' | 'r' | 's'  => '7'
-      case 't' | 'u' | 'v'  => '8'
-      case 'w' | 'x' | 'y'  => '9'
-      case 'o' | 'q' | 'z'  => '0'
-    }
+case class DictItem(word: String, number: String)
+
+case class PartialSolution(position: Int, words: List[String])
+
+class Dictionary(source: List[String]) {
+  val items: List[DictItem] = distinctByNumber(source.map(genDictItem))
+
+  private def genDictItem(word: String): DictItem = {
+    DictItem(
+      word,
+      word.map {
+        case 'i' | 'j'        => '1'
+        case 'a' | 'b' | 'c'  => '2'
+        case 'd' | 'e' | 'f'  => '3'
+        case 'g' | 'h'        => '4'
+        case 'k' | 'l'        => '5'
+        case 'm' | 'n'        => '6'
+        case 'p' | 'r' | 's'  => '7'
+        case 't' | 'u' | 'v'  => '8'
+        case 'w' | 'x' | 'y'  => '9'
+        case 'o' | 'q' | 'z'  => '0'
+      }
+    )
   }
 
-  def prepareDict(raw: List[String]): List[(String, String)] = raw.map { x => (x, wordToNumbers(x)) }
-
-  def solve(number: String, dict: List[(String, String)]): List[String] = {
-    def numberBeginsWith(numericWord: String): Boolean =
-      (number.size >= numericWord.size) &&
-        (number.substring(0, numericWord.size) == numericWord)
-
-    def generateCandidate(dictEntry: (String, String)): List[String] = {
-      val (word, numericWord) = dictEntry
-
-      if (numberBeginsWith(numericWord))
-        if (number.size == numericWord.size)
-          List(word)
-        else
-          solve(number.substring(numericWord.size), dict) match {
-            case Nil    => Nil
-            case result => word :: result
-          }
-      else
-        Nil
+  private def distinctByNumber(list: List[DictItem]): List[DictItem] = {
+    val seen = mutable.HashSet[String]()
+    list.flatMap { x =>
+      if (seen(x.number))
+        None
+      else {
+        seen += x.number
+        Some(x)
+      }
     }
+  }
+}
 
-    def cmpF(arg: List[String]): Int = {
-      arg.size match {
-        case 0   => Int.MaxValue
-        case any => any
+object Problem1002 {
+
+  def solve(number: String, dict: Dictionary): String = {
+    type State    = List[PartialSolution]
+    type Solution = List[String]
+
+    val positionSeen = mutable.HashSet[Int]()
+
+    @tailrec
+    def producePartialState(ps: PartialSolution, init: State = Nil, items: List[DictItem] = dict.items)
+    : Either[State, Solution] = {
+      items match {
+        case Nil => Left(init)
+        case _   =>
+          val item = items.head
+          if (number.startsWith(item.number, ps.position)) {
+            if (ps.position + item.number.size == number.size)
+              Right((item.word :: ps.words).reverse)
+            else {
+              val newPosition = ps.position + item.number.length
+              if (positionSeen(newPosition)) {
+                producePartialState(ps, init, items.tail)
+              }
+              else {
+                positionSeen += newPosition
+                val generated = PartialSolution(newPosition, item.word :: ps.words)
+                producePartialState(ps, generated :: init, items.tail)
+              }
+            }
+          }
+          else
+            producePartialState(ps, init, items.tail)
       }
     }
 
-    dict.map(generateCandidate).minBy(cmpF)
+    @tailrec
+    def produceNextState(state: State, init: State = Nil): Either[State, Solution] = state match {
+      case Nil => Left(init)
+      case _   =>
+        producePartialState(state.head) match {
+          case Left(partialState) => produceNextState(state.tail, partialState ::: init)
+          case Right(solution)    => Right(solution)
+        }
+    }
+
+    def BFS(state: State): Solution = {
+      state match {
+        case Nil => Nil
+        case _ =>
+          produceNextState(state) match {
+            case Left(newState)   => BFS(newState)
+            case Right(solution)  => solution
+          }
+      }
+    }
+
+    val initialState: State = List(PartialSolution(0, Nil))
+    BFS(initialState) match {
+      case Nil => "No solution."
+      case any => any.mkString(" ")
+    }
   }
 
   @tailrec
@@ -57,10 +111,7 @@ object Problem1002 {
     val dictSize = readLine().toInt
     val rawDict = List.fill(dictSize) { readLine() }
 
-    solve(number, prepareDict(rawDict)) match {
-      case Nil      => println("No solution.")
-      case result   => println(result.mkString(" "))
-    }
+    println(solve(number, new Dictionary(rawDict)))
 
     processData()
   }
